f16d719d77ac53d1534e71aab3acf0ecb1a555b8
veer bahadur tiwari <veeru.ramjas@gmail.com>
2023-03-14 09:19:18 +0530

    Initial Commit

---
+++ iOSExercise/iOSExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSExerciseApp.swift
+//  iOSExercise
+//
+//  Created by mac on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
